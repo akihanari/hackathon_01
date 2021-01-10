f9fd92d37ed8975df8eb6db00a3e0bf49f065856
akihanari <akihanari@gmail.com>
2021-01-10 14:36:12 +0900

    cat add

--- space.js
+++ space.js
@@ -5,18 +5,20 @@ let img;
 //let fr = 30; //fps
 
 function preload() {
-  img = loadImage('images/PB256850o_TP_V.jpg');
+  img = loadImage('images/cat.png');
 }
 
 function setup() {
-  createCanvas(400,400);
-  image(img,200,200);
+//   createCanvas(400,400);
+  createCanvas(windowWidth, windowHeight);
+
   background(0,4,51);
+  image(img,windowWidth/2,windowHeight - 123);
   //framerate(fr);
   stroke(255);
   for (i=0; i<50; i++){
-    x = random(0,400);
-    y = random(0,400);
+    x = random(0,windowWidth);
+    y = random(0,windowHeight);
     s = random(5,10);
     //point(x, y);
     ellipse(x, y, s);
