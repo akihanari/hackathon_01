e506a4e0d679255e7ab7b89fd12760b372bdfe94
akihanari <akihanari@gmail.com>
2021-01-10 15:44:44 +0900

    cat change

--- space.js
+++ space.js
@@ -5,7 +5,7 @@ let img;
 //let fr = 30; //fps
 
 function preload() {
-  img = loadImage('images/cat.png');
+  img = loadImage('images/cat02.png');
 }
 
 function setup() {
@@ -13,16 +13,22 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   background(0,4,51);
-  image(img,windowWidth/2,windowHeight - 123);
+
   //framerate(fr);
-  stroke(255);
+  stroke(250);
   for (i=0; i<50; i++){
     x = random(0,windowWidth);
     y = random(0,windowHeight);
     s = random(5,10);
+    drawingContext.shadowBlur = 25;
+    drawingContext.shadowOffsetX = 0;
+    drawingContext.shadowOffsetY = 0;
+    drawingContext.shadowColor = color(240,240,255);
     //point(x, y);
     ellipse(x, y, s);
   }
+  image(img,windowWidth/2,windowHeight - 406);
+//   filter(BLUR, 0);
 }
 
 function draw() {
