e82a406ee35691b0f542f05d2701fceae09bd05f
akihanari <akihanari@gmail.com>
2021-01-10 16:22:43 +0900

    star add

--- space.js
+++ space.js
@@ -1,6 +1,7 @@
 let x,y;
-let s;
+let w;
 let img;
+let h,s,b;
 //let count = 0;
 //let fr = 30; //fps
 
@@ -12,21 +13,35 @@ function setup() {
 //   createCanvas(400,400);
   createCanvas(windowWidth, windowHeight);
 
-  background(0,4,51);
+//   background(0,4,51);
+  background(0);
 
   //framerate(fr);
-  stroke(250);
-  for (i=0; i<50; i++){
+//   stroke(250);
+  for (i=1; i<200; i++){
+    blendMode(SCREEN);
+    colorMode(HSB);
+    h = random(100,360);
+    fill(h, 100, 100, 100);
+
     x = random(0,windowWidth);
     y = random(0,windowHeight);
-    s = random(5,10);
-    drawingContext.shadowBlur = 25;
-    drawingContext.shadowOffsetX = 0;
-    drawingContext.shadowOffsetY = 0;
-    drawingContext.shadowColor = color(240,240,255);
-    //point(x, y);
-    ellipse(x, y, s);
+    w = random(2,8);
+    // drawingContext.shadowBlur = 25;
+    // drawingContext.shadowOffsetX = 0;
+    // drawingContext.shadowOffsetY = 0;
+    // drawingContext.shadowColor = color(240,240,255);
+    strokeWeight(i*0.5);
+    stroke(
+          map(i, 1, 50, 180, 360),
+          80,
+          map(i, 1, 50, 15, 1),
+          100
+          );
+    ellipse(x, y, w);
   }
+  blendMode(BLEND);
+  colorMode(RGB);
   image(img,windowWidth/2,windowHeight - 406);
 //   filter(BLUR, 0);
 }
